8fe0c45009f180e7ae3baeee7d4a66f4282f1ec3
Armen MAHMEDOV <armen.mahmedov@gmail.com>
2018-11-15 11:59:41 +0100

    Add files via upload

---
+++ DiviaHoraire/DiviaHoraire/HomeParserXML.swift
@@ -0,0 +1,121 @@
+//
+//  HomeParserXML.swift
+//  DiviaHoraire
+//
+//  Created by esirem on 15/11/2018.
+//  Copyright © 2018 esirem. All rights reserved.
+//
+
+import Foundation
+
+struct Ligne {
+    var code: String
+    var nom: String
+    var sens: String
+    var vers: String
+}
+
+class HomeParser: NSObject, XMLParserDelegate{
+    
+    var ligneItems: [Ligne] = []
+    private var currentElement = ""
+    
+    private var currentCode: String = "" {
+        didSet {
+            currentCode = currentCode.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        }
+    }
+    
+    private var currentNom: String = "" {
+        didSet {
+            currentNom = currentNom.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        }
+    }
+    
+    private var currentSens: String = "" {
+        didSet {
+            currentSens = currentSens.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        }
+    }
+    
+    private var currentVers: String = "" {
+        didSet {
+            currentVers = currentVers.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        }
+    }
+    
+    private var parserCompletionHandler:(([Ligne]) -> Void)?
+    
+    func parseFeed(url: String, completionHandler: (([Ligne]) -> Void)?){
+        self.parserCompletionHandler = completionHandler
+        
+        let request = URLRequest(url: URL(string: url)!)
+        let urlSession = URLSession.shared
+        let task = urlSession.dataTask(with: request) { (data, response, error) in
+            guard let data = data else {
+                if let error = error {
+                    print(error.localizedDescription)
+                }
+                return
+            }
+            let parser = XMLParser(data: data)
+            parser.delegate = self
+            parser.parse()
+        }
+        task.resume()
+    }
+    
+//    var myData: Data = "".data(using: .ascii)!
+//
+//
+//    func setData(data: Data) -> Void {
+//        myData = data
+//    }
+//
+//    func parse() {
+//        let parser = XMLParser(data: myData)
+//        parser.delegate = self
+//        parser.parse()
+//    }
+//
+    func parserDidEndDocument(_ parser: XMLParser) {
+        print("end")
+        parserCompletionHandler?(ligneItems)
+    }
+    
+    func parserDidStartDocument(_ parser: XMLParser) {
+        print("start")
+    }
+    
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        currentElement = elementName
+        if currentElement == "ligne" {
+            currentNom = ""
+            currentCode = ""
+            currentVers = ""
+            currentSens = ""
+        }
+    }
+    
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        switch currentElement {
+            case "code": currentCode += string
+            case "nom": currentNom += string
+            case "sens": currentSens += string
+            case "vers": currentVers += string
+            default: break
+        }
+    }
+
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        if elementName == "ligne" {
+            let ligneIterm = Ligne(code: currentCode, nom: currentNom, sens: currentSens, vers: currentVers)
+            self.ligneItems.append(ligneIterm)
+        }
+    }
+    
+    func parser(_ parser: XMLParser, parseErrorOccurred parseError: Error) {
+        print(parseError.localizedDescription)
+    }
+    
+}
--- DiviaHoraire/DiviaHoraire/ViewController.swift
+++ DiviaHoraire/DiviaHoraire/ViewController.swift
@@ -8,41 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController, XMLParserDelegate {
+class ViewController: UIViewController {
 
     //https://www.youtube.com/watch?v=8IBFuBgM09A
+    
+    
+    //https://www.youtube.com/watch?v=fP69LI5bZlg
     var urlLink: String = ""
+    
+    var lignes: [Ligne]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        urlLink = "http://timeo3.keolis.com/relais/217.php?xml=1"
-        guard let url = URL(string: urlLink) else { return }
+        fetchData()
         
-        URLSession.shared.dataTask(with: url) { (data, response, error) in
-            guard let data = data, error == nil else {
-                print(error ?? "Unknown error")
-                return
-            }
-            // Convert data to String
-            var stringData: String = String(data: data, encoding: .ascii)!
-            
-            // Convert LF or CR
-            stringData = stringData.replacingOccurrences(of: "\r", with: "\n")
-            
-            print(stringData)
-            
-            
-            
-//            let parser = XMLParser(data: data)
-//            parser.delegate = self
-//            if parser.parse() {
-//               // print(self.results ?? "No results")
+//        urlLink = "http://timeo3.keolis.com/relais/217.php?xml=1"
+//        guard let url = URL(string: urlLink) else { return }
+//
+//        URLSession.shared.dataTask(with: url) { (data, response, error) in
+//            guard let data = data, error == nil else {
+//                print(error ?? "Unknown error")
+//                return
 //            }
-        }.resume()
+//            // Convert data to String
+//            var stringData: String = String(data: data, encoding: .ascii)!
+//
+//            let homeParser = ParserHome()
+//
+//
+//            // Convert LF or CR
+//            stringData = stringData.replacingOccurrences(of: "\r", with: "\n")
+//
+//            //print(stringData)
+//
+//            
+//
+////            let parser = XMLParser(data: data)
+////            parser.delegate = self
+////            if parser.parse() {
+////               // print(self.results ?? "No results")
+////            }
+//        }.resume()
     }
 
+    func fetchData() {
+        let homeParser = HomeParser()
+        homeParser.parseFeed(url: "http://timeo3.keolis.com/relais/217.php?xml=1", completionHandler: <#T##(([Ligne]) -> Void)?##(([Ligne]) -> Void)?##([Ligne]) -> Void#>)
+        // video 25:56
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
