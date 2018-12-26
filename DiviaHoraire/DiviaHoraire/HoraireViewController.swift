b1313d7e13ba05c999ec1ec71ecbad520cb29d22
armen <armen@Mac-de-armen.local>
2018-12-26 19:01:40 +0100

    finish horaire parse TODO: layout

--- DiviaHoraire/DiviaHoraire/HoraireViewController.swift
+++ DiviaHoraire/DiviaHoraire/HoraireViewController.swift
@@ -17,6 +17,11 @@ class HoraireViewController: UIViewController {
     
     var horairesItems: [Horaire]?
     
+    @IBOutlet weak var nomLabel: UILabel!
+    @IBOutlet weak var ligneLabel: UILabel!
+    
+    @IBOutlet weak var passage1Label: UILabel!
+    @IBOutlet weak var passage2Label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,7 +30,10 @@ class HoraireViewController: UIViewController {
         //print(code)
         print(nom)
         print(ligne)
+        nomLabel.text = nom
+        ligneLabel.text = ligne
         fetchData()
+     
         // http://timeo3.keolis.com/relais/217.php?xml=3&ran=1&refs=274433027|274433539|274434307
     }
     
@@ -34,13 +42,35 @@ class HoraireViewController: UIViewController {
         horaireParser.parseFeed(url: "http://timeo3.keolis.com/relais/217.php?xml=3&ran=1&refs=\(refs)")
         { (horairesItems) in
             self.horairesItems = horairesItems
-            //print(self.horairesItems!)
+            print(self.horairesItems!)
+            print(self.horairesItems!.count)
             OperationQueue.main.addOperation {
-                //self.arretsTableView.reloadSections(IndexSet(integer: 0), with: .left)
+                if self.horairesItems!.isEmpty {
+                    self.setPassage1ToNull()
+                    self.setPassage2ToNull()
+                } else {
+                    if self.horairesItems!.count == 1 {
+                        self.passage1Label.text = self.horairesItems![0].duree
+                    }else if self.horairesItems!.count == 2 {
+                        self.passage1Label.text = self.horairesItems![0].duree
+                        self.passage2Label.text = self.horairesItems![1].duree
+                    }else {
+                        self.setPassage1ToNull()
+                        self.setPassage2ToNull()
+                    }
+                }
             }
         }
     }
 
+    func setPassage1ToNull() {
+        self.passage1Label.text = "Pas de passage !"
+    }
+    
+    func setPassage2ToNull() {
+        self.passage2Label.text = "Pas de passage !"
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
